dd0a9e303e588c8a07fb6133589ed1b3dd3bf738
Darth Vader <33085599+the-darklord@users.noreply.github.com>
2023-01-03 05:25:18 +0000

    NUMPY

--- Python/NUMPY/statistics.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-data=[1,2,3,4,5,6,7,8,9,10]
-
-print("Mean : ",np.mean(data))
-print("Median : ",np.median(data))
-print("Percentile 25 : ",np.percentile(data,25),"\npercentile 75 : ",np.percentile(data,75))
-print("Variance : ",np.var(data))
-print("Standard Deviation : ",np.std(data))
