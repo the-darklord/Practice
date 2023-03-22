1473e491a2f9744cbc8cf346c9bbe81359af5fea
THEDARKLORD <saimihirnath@protonmail.com>
2023-03-23 00:08:24 +0530

    EMBEDDED C

---
+++ Embedded C/CODES/ringcounter.c
@@ -0,0 +1,30 @@
+#include <LPC17xx.h>
+
+unsigned int i,j,c=1;
+
+int main(void)
+{
+	SystemInit();
+	SystemCoreClockUpdate();
+	
+	LPC_PINCON->PINSEL0 &= 0xFF0000FF;
+	LPC_GPIO0->FIODIR |= 0X00000FF0;
+	LPC_PINCON->PINSEL4 &= 0xFCFFFFFF;
+	LPC_GPIO2->FIODIR &= 0XFFFFFFFE;
+	
+	while(1)
+	{
+		if(LPC_GPIO2->FIOPIN&1)
+		{
+			for(i=0;i<8;i++)
+			{
+				LPC_GPIO0->FIOPIN = c<<4;
+				for(j=0;j<10000;j++);
+				c=c<<1;
+			}
+			c=1;
+		}
+	}
+	
+	return 0;
+}
